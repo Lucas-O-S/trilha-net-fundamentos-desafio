d66cd775caeffc1637adf577603e061362282a36
Lucas-O-S <lucasoscomercial@gmail.com>
2023-12-20 16:06:33 -0300

    Codigo alterado

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,54 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
+        //Preço inicial que será somado independente de quantas horas o veiculo fique
         private decimal precoInicial = 0;
+        
+        //Preço por hora estacionada
         private decimal precoPorHora = 0;
+        
+        //Lista de placas de veiculos
         private List<string> veiculos = new List<string>();
 
+        //Construtor de veiculos estacionados
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
         }
 
+        //Adiciona um veiculo a lista 
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.Write("Digite a placa do veículo para estacionar: ");
+            try
+            {
+                string Placa = Console.ReadLine();
+
+                //Formata a placa para o formato pedido
+                Placa = Verificador.AjustarPlaca(Placa);
+
+                //Verifica se o tamanho da placa está correto 7 digitos(ex: ABCDEFG)
+                if (Verificador.VerificarTamanho(Placa))
+                {
+                    throw new Exception("Tamanho da String fora do padrão, verifique se há mais ou menos de 7 digitos a placa(Disconsiderando ' ' ou '-')");
+                }
+
+                //Verifica se há repetição de veiculos 
+                if (Verificador.VerificarRepetição(veiculos, Placa))
+                {
+                    throw new Exception("Carro já foi cadastrado");
+                }
+
+                //Adiciona veiculo a lista
+                veiculos.Add(Placa);
+
+
+            }
+
+            //Pega possiveis erros
+            catch(Exception e) {
+                Console.WriteLine($"Erro encontrado: {e}");
+                
+            }
+
         }
 
+        //Remove veiculos da lista e calcula preço a ser pago
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+            Console.Write("Digite a placa do veículo para remover: ");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
 
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            try
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                placa = Console.ReadLine();
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                //Formata a placa
+                placa = Verificador.AjustarPlaca(placa);
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                //Verifica tamanho da placa
+                if (Verificador.VerificarTamanho(placa))
+                {
+                    throw new Exception("Tamanho da String fora do padrão, verifique se há mais ou menos de 7 digitos a placa(Disconsiderando ' ' ou '-')");
+                }
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                // Verifica se o veículo existe
+                if (veiculos.Any(x => x == placa))
+                {
+                    //Separei o calculo do preço em uma outra função
+                    CalcularPreco(placa);
+                }
+                else
+                {
+                    throw new Exception ("Veiculo Não existe");
+                }
             }
-            else
+
+            catch(FormatException e)
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine($"Formato digitado esta errado: {e}");
             }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro encontrado: {e}");
+
+            }
+
+            
         }
 
+        private void CalcularPreco(string placa)
+        {
+
+            Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+
+            // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+            // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+            // *IMPLEMENTE AQUI*
+
+            //Horas estacionadas
+            int horas = 0;
+
+            //Preço total a se calculado
+            decimal valorTotal = 0;
+
+            //Variavel temporaria das horas
+            string StringHoras = Console.ReadLine();
+
+            //Converte para int dentro da variavel horas
+            if (int.TryParse(StringHoras, out int Teste))
+            {
+                horas = Teste;
+            }
+            //Verifica o erro
+            else
+            {
+                throw new Exception("As horas digitadas estão em formato errado");
+            }
+
+            //Verifica a possibilidade de erros com horas negativas
+            if (horas < 0)
+            {
+                throw new Exception("Hora está negativa, tente outro valor");
+            }
+
+            //Calcula as horas totais
+            valorTotal = horas * precoPorHora + precoInicial;
+
+            // TODO: Remover a placa digitada da lista de veículos
+            // *IMPLEMENTE AQUI*
+
+            //Remove a placa
+            veiculos.Remove(placa);
+
+
+            //Resposta
+            Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+
+        }
+
+        //Lista as placas cadastradas
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
@@ -57,6 +169,12 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+
+                //Escreve todas as placas
+                foreach (string Placa in veiculos)
+                {
+                    Console.WriteLine(Placa);
+                }
             }
             else
             {
---
+++ DesafioFundamentos/Models/Verificador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+
+    //Classe estatica para verificar detalhes
+    public static class Verificador
+    {
+        //Verifica tamanho da placa, deve ter 7 digitos(Ex: AAAAAAA)
+        public static bool VerificarTamanho(string Placa)
+        {
+
+            if (Placa.Length > 7 || Placa.Length < 7)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //Verifica se a placa já foi cadastrada
+        public static bool VerificarRepetição(List<String> Veiculos, String Placa)
+        {
+
+            foreach (String Carro in Veiculos)
+            {
+                if (Carro == Placa)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //Ajusta o formato da placa eliminando espaços e traços
+        public static string AjustarPlaca(string Placa)
+        {
+            Placa = Placa.ToUpper();
+
+            Placa = Placa.Replace(" ", "");
+
+
+            Placa = Placa.Replace("-", "");
+
+            return Placa;
+
+        }
+    }
+}
